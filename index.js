8a731f1cb2aa2d3fef7d260e82958622942a5241
Kelvin Ho <kelvinho84@gmail.com>
2021-10-20 17:11:03 +0800

    add feature

--- index.js
+++ index.js
@@ -74,6 +74,44 @@ googleTrendRouter.get('/realTimeTrends', async function (req, res) {
   }
 });
 
+googleTrendRouter.get('/interestOverTime', async function (req, res) {
+  console.log(req.query);
+  if (
+    req.query.hasOwnProperty('geo') &&
+    !GEO_CODE_ONLY.includes(req.query?.geo)
+  ) {
+    res.status(422).send({ error: 'Invalid GEO code' });
+  }
+  if (!req.query.hasOwnProperty('keyword')) {
+    res.status(422).send({ error: 'Missing keyword' });
+  }
+
+  const form = {
+    geo: req.query?.geo || 'US',
+    keyword: req.query.keyword,
+    ...(req.query.hasOwnProperty('category')
+      ? { category: parseInt(req.query.category) }
+      : {}),
+    ...(req.query.hasOwnProperty('startTime')
+      ? { startTime: parseInt(req.query.startTime) }
+      : {}),
+    ...(req.query.hasOwnProperty('endTime')
+      ? { endTime: parseInt(req.query.endTime) }
+      : {}),
+  };
+  console.log('form', form);
+  try {
+    const data = await googleTrends.interestOverTime(form);
+    // console.log(data);
+    if (data) {
+      res.setHeader('Content-Type', 'application/json');
+      res.json(JSON.parse(data));
+    }
+  } catch (err) {
+    res.status(500).send(err);
+  }
+});
+
 app.use('/gt', googleTrendRouter);
 
 app.listen(port, () => {
