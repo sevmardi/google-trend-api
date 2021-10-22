6bcfed8bb3b4b6f4f5f3b3d34fba82ffe025687a
Kelvin Ho <kelvinho84@gmail.com>
2021-10-22 17:18:54 +0800

    add tts

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const port = process.env.PORT || 3000;
 const googleTrendRouter = express.Router();
 const googleTrends = require('google-trends-api');
 
+const ttsRouter = express.Router();
+
 const GEO = require('./data/geo.json');
 const GEO_CODE_ONLY = Object.keys(GEO);
 const CATEGORY = require('./data/category.json');
@@ -113,6 +115,80 @@ googleTrendRouter.get('/interestOverTime', async function (req, res) {
 });
 
 app.use('/gt', googleTrendRouter);
+
+ttsRouter.get('/speech/lang', function (req, res) {
+  const LANGUAGES = {
+    af: 'Afrikaans',
+    sq: 'Albanian',
+    ar: 'Arabic',
+    hy: 'Armenian',
+    ca: 'Catalan',
+    zh: 'Chinese',
+    'zh-cn': 'Chinese (Mandarin/China)',
+    'zh-tw': 'Chinese (Mandarin/Taiwan)',
+    'zh-yue': 'Chinese (Cantonese)',
+    hr: 'Croatian',
+    cs: 'Czech',
+    da: 'Danish',
+    nl: 'Dutch',
+    en: 'English',
+    'en-au': 'English (Australia)',
+    'en-uk': 'English (United Kingdom)',
+    'en-us': 'English (United States)',
+    eo: 'Esperanto',
+    fi: 'Finnish',
+    fr: 'French',
+    de: 'German',
+    el: 'Greek',
+    ht: 'Haitian Creole',
+    hi: 'Hindi',
+    hu: 'Hungarian',
+    is: 'Icelandic',
+    id: 'Indonesian',
+    it: 'Italian',
+    ja: 'Japanese',
+    ko: 'Korean',
+    la: 'Latin',
+    lv: 'Latvian',
+    mk: 'Macedonian',
+    no: 'Norwegian',
+    pl: 'Polish',
+    pt: 'Portuguese',
+    'pt-br': 'Portuguese (Brazil)',
+    ro: 'Romanian',
+    ru: 'Russian',
+    sr: 'Serbian',
+    sk: 'Slovak',
+    es: 'Spanish',
+    'es-es': 'Spanish (Spain)',
+    'es-us': 'Spanish (United States)',
+    sw: 'Swahili',
+    sv: 'Swedish',
+    ta: 'Tamil',
+    th: 'Thai',
+    tr: 'Turkish',
+    vi: 'Vietnamese',
+    cy: 'Welsh',
+  };
+  return res.json(LANGUAGES);
+});
+
+// ** tts ** //
+
+ttsRouter.get('/speech', function (req, res) {
+  if (!req.query.hasOwnProperty('text')) {
+    res.status(422).send({ error: 'Missing text' });
+  }
+  const form = {
+    lang: req.query?.lang || 'en-us',
+    text: req.query.text,
+  };
+
+  var gtts = require('node-gtts')(form.lang);
+  res.set({ 'Content-Type': 'audio/mpeg' });
+  gtts.stream(form.text).pipe(res);
+});
+app.use('/tts', ttsRouter);
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
